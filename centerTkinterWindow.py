dd1d08510125e387561abd9cb1ebd0e6b6bdaba7
itzmudassir <itzmudassir07@gmail.com>
2024-10-07 10:34:49 +0500

    Two methods to center Tkinter Window

---
+++ centerTkinterWindow.py
@@ -0,0 +1,27 @@
+# import tkinter as tk
+ 
+# root = tk.Tk()
+
+# width = 400 # Width 
+# height = 400 # Height
+ 
+# screen_width = root.winfo_screenwidth()  # Width of the screen
+# screen_height = root.winfo_screenheight() # Height of the screen
+ 
+# # Calculate Starting X and Y coordinates for Window
+# x = (screen_width/2) - (width/2)
+# y = (screen_height/2) - (height/2)
+ 
+# root.geometry('%dx%d+%d+%d' % (width, height, x, y))
+# root.mainloop()
+
+
+# import tkinter as tk
+ 
+# root = tk.Tk()
+ 
+# label = tk.Label(root, text = "A Window")
+# label.place(x = 70, y = 80)
+ 
+# root.eval('tk::PlaceWindow . center')
+# root.mainloop()
